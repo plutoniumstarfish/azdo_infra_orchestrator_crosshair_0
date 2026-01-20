b434270d7c60904cbb93eb732f5b009d749e9391
plutoniumstarfish <plutoniumstarfish@yahoo.com>
2026-01-20 23:51:26 +0800

    Update TF_VARS path in dev1.sh

--- config_kalashnikov/env/dev1.sh
+++ config_kalashnikov/env/dev1.sh
@@ -1,4 +1,4 @@
 export BUCKET_NAME="counterstrike-dev-use2-deployment-package"
 export ENV_PACKAGE="dev1_v1"
 export ENV_IAC="dev1_use2_v1"
-export TF_VARS="/opt/agent/dev1_use2_v1.tfvars"
+export TF_VARS="/opt/agent/tfvars/dev1_use2_v1.tfvars"
